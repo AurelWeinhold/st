741886abb1547f6d517c78fb8af934a7d65fa8e3
Aurel Weinhold <aurel@weinhold.org>
2021-10-11 15:35:06 +0200

    Switch to Hack font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Inconsolata Nerd Font Mono:size=12";
+static char *font = "Hack Nerd Font Mono:size=10";
 static char *font2 = "Material Icons:size=12";
 static int borderpx = 2;
 
